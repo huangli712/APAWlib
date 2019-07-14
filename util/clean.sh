8e2f0c4c64574d1fe1581a6cf91b7d3ccc17e1f4
Li Huang <lihuang.dmft@gmail.com>
2019-07-14 20:04:16 +0800

    add pdf support for clean.sh

--- util/clean.sh
+++ util/clean.sh
@@ -30,5 +30,5 @@
 ##
 ##
 
-rm -fr w* t* l* pot* r* *.xml *.abinit expot dummy density vloc NC OCCWFN
+rm -fr w* t* l* pot* r* *.xml *.abinit *.pdf expot dummy density vloc NC OCCWFN
 rm -fr Fr Ra Ac Th Pa U Np Pu Am Cm Bk Cf Es Fm Md No Lr Rf
